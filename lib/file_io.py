7f376009f530e7541ed115bfc8b1fc8fcb36e6b0
Joy Olago <joydanielle62@gmail.com>
2023-08-30 17:14:58 +0300

    Completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,18 @@
+from pathlib import Path
+
 def write_file(file_name, file_content):
-    pass
+    file_path = Path(file_name).resolve()  # Convert the Path object to a string
+    with open(file_path.with_suffix(".txt"), "w") as file:
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    file_path = Path(file_name).resolve()  # Convert the Path object to a string
+    with open(file_path.with_suffix(".txt"), "a") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    file_path = Path(file_name).resolve()  # Convert the Path object to a string
+    with open(file_path.with_suffix(".txt"), "r") as file:
+        content = file.read()
+    return content
